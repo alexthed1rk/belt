15c56e249655895298d6e2550ce114550a09a658
Alexander Zhura <nice.zhura@list.ru>
2026-08-21 12:31:32 +0300

    Impl: code cleanup

--- benchmark/belt_benchmark.sh
+++ benchmark/belt_benchmark.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # -sanitize:address
-build_flags="-o:aggressive -disable-assert"
+build_flags="-o:speed -disable-assert -show-timings"
 vet_flags="-strict-style -vet-tabs -disallow-do -warnings-as-errors"
-define_flags="-define:ODIN_TEST_THREADS=1 -define:ODIN_TEST_RANDOM_SEED=2048 -define:ODIN_TEST_TRACK_MEMORY=false"
+define_flags="-define:ODIN_TEST_THREADS=0 -define:ODIN_TEST_RANDOM_SEED=2048 -define:ODIN_TEST_TRACK_MEMORY=false"
 odin test . $build_flags $vet_flags $define_flags
